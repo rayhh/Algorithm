679040ce381cf091d1869bdc5a0009402800abe8
Ray_h <leihao22@126.com>
2020-02-18 23:04:50 +0800

    topK问题的部分快速排序解法（与快速中位数算法相似）

---
+++ topk_quick_sort.cpp
@@ -0,0 +1,18 @@
+//求一个数组中的前K小(大)的数
+
+void topk(vector<int>& input, int low, int high){
+	if(low>=high) return;
+	int mid=input[low];
+	int left=low;
+	int right=high;
+	while(left<right){
+		while(left<right && input[right]>=mid) right++;
+		input[left]=input[right];
+		while(left<right && input[left]<=mid) left--;
+		input[right]=input[left];
+	}
+	input[left]=mid;
+	if(left+1==k) return;
+	else if(left>=k) topk(input,low,left-1,k);
+	else topk(input,left+1,high,k-left-1);
+}
