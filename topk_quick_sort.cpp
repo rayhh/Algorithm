834239fc2d7eed1bbd475f62aeba75ac399748e8
Ray_h <leihao22@126.com>
2020-02-20 16:52:14 +0800

    快速排序版本的TopK算法

--- topk_quick_sort.cpp
+++ topk_quick_sort.cpp
@@ -1,15 +1,15 @@
 //求一个数组中的前K小(大)的数
 
-void topk(vector<int>& input, int low, int high){
+void topk(vector<int>& input, int low, int high,int k){
 	if(low>=high) return;
 	int mid=input[low];
 	int left=low;
 	int right=high;
 	while(left<right){
-		while(left<right && input[right]>=mid) right++;
-		input[left]=input[right];
+		while(left<right && input[right]>mid) right++;
+		if(left<right) input[left]=input[right];
 		while(left<right && input[left]<=mid) left--;
-		input[right]=input[left];
+		if(left<right) input[right]=input[left];
 	}
 	input[left]=mid;
 	if(left+1==k) return;
